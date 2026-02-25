e89172944abed39c8b956d1ae07f51b23dd39866
Raiane Cardoso <166438302+raianecardoso@users.noreply.github.com>
2026-02-25 18:41:41 +0000

    Add conteúdo aula 012 - condicionais. See #158

---
+++ aulas/012/app.py
@@ -0,0 +1,20 @@
+# Se o dia está quente 
+# O dia está quente
+# Beba água.
+# Se o dia está frio.
+# O dia está frio. 
+# Use agasalho.
+# Se o dia não está quente, nem frio
+# é um belo dia!
+
+esta_quente = False
+esta_frio =  False
+
+if esta_quente:
+    print("O dia está quente")
+    print("Beba água")
+elif esta_frio:
+    print("O dia está frio")
+    print("Use agasalho")
+else:
+    print("É um lindo dia")
