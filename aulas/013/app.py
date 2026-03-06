649137cedbc4d9b5ac75fe24da188ea31ca579e8
Raiane Cardoso <166438302+raianecardoso@users.noreply.github.com>
2026-03-06 16:58:55 +0000

    Adc conteúdo aula 013 - operadores logicos.

---
+++ aulas/013/app.py
@@ -0,0 +1,9 @@
+# and or not 
+# se ele possui alta renda
+# se ele possui bom crédito 
+
+tem_alta_renda = True
+tem_historico_criminal = True
+
+if tem_alta_renda and not tem_historico_criminal:
+    print("Elegível para empréstimo!")
