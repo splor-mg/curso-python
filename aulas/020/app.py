bb6a3cc3bb6c8e9d46ae3231e4cdf80e162ddd39
Raiane Cardoso <166438302+raianecardoso@users.noreply.github.com>
2026-03-25 19:17:26 +0000

    Adc conteudo da aula 20 - metodos de listas

---
+++ aulas/020/app.py
@@ -0,0 +1,16 @@
+# métodos de listas 
+# append = adc itens
+# insert = adc itens na posição index
+# remove = remove um item
+# clear = limpa a lista
+# pop = exclui o último item
+# index = se o número existe
+# count = quantas vezes um valor aparece
+# sort = ordenar a lista em ordem crescente 
+# reverse = inverte a ordem dos itens a lista 
+# copy = copia a lista
+
+numeros = [7, 5, 8, 2, 0, 8]
+numeros2 = numeros.copy()
+numeros.append(1)
+print(numeros2)
