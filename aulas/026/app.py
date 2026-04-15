61fc27f37d7816d8170e6d25e3143e6c7c158a27
Raiane Cardoso <166438302+raianecardoso@users.noreply.github.com>
2026-04-15 14:20:49 +0000

    Add conteudo da auma 6-keyword arguments.

---
+++ aulas/026/app.py
@@ -0,0 +1,9 @@
+def boas_vidas(nome, sobrenome):
+    print(f"Olá {nome} {sobrenome}")
+    print("Seja bem vinda")
+
+def custo(total)
+print("início")
+boas_vidas("Abreu", "Maria")
+custo(100, entrega=20, desconto=0.1)
+print("fim")
