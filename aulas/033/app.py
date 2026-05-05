0ad6213b17faf10c969defb1831e1dfc6f920a5f
Raiane Cardoso <166438302+raianecardoso@users.noreply.github.com>
2026-05-05 14:17:37 +0000

    Adc cnteúdo aula 033.

---
+++ aulas/033/app.py
@@ -0,0 +1,7 @@
+import conversores
+from conversores import lbs_para_kg
+
+print(conversores.kg_para_lbs(10))
+
+print(lbs_para_kg(100))
+
---
+++ aulas/033/conversores.py
@@ -0,0 +1,5 @@
+def lbs_para_kg(peso):
+    return peso * 0.45
+
+def kg_para_lbs(peso):
+    return peso / 0.45
