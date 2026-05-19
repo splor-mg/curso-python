9b338b704aa3eb81b4a86f4e88927860fa3df322
Raiane Cardoso <166438302+raianecardoso@users.noreply.github.com>
2026-05-19 15:43:47 +0000

    Adc conteúdo sobre break e continue

--- aulas/016/app.py
+++ aulas/016/app.py
@@ -1,3 +1,13 @@
-for item in range(1, 10, 3):
-    print(item)
+#for item in range(1, 10, 3):
+#    print(item)
+
+# break = interrompe o nosso loop
+# continue = pula a iteração
+
+for numero in range(1,6):
+    if numero == 3:
+        continue
+    print(numero)
+
+print('fim')
 
